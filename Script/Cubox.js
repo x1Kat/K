9b98ffa4141d01f0809781133a9ee07a5cfcda0f
iKath <42527047+iKath@users.noreply.github.com>
2023-08-05 00:38:53 +0800

    Create Cubox.js

---
+++ Script/Cubox.js
@@ -0,0 +1,18 @@
+/*
+
+Cubox：https://apps.apple.com/app/id1113361350
+
+[rewrite_local]
+^https?:\/\/cubox\.(cc|pro)\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Cubox.js
+
+[MITM]
+hostname = cubox.cc, cubox.pro
+
+*/
+var kath = JSON.parse($response.body);
+kath.data.isExpire = false;
+kath.data.expireTime ="8888-08-08T08:08:08Z";
+kath.data.nickName ="https://t.me/Guding88";
+kath.data.level = 1;
+kath.data.paymentSource = 9;
+$done({ body: JSON.stringify(kath) });
